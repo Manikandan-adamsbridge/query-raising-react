b216e11e965a20bc850842a478ad2b1a246efbcd
ManiKandan2129 <gokulvirat2129@gmail.com>
2025-01-23 21:20:02 +0530

    project creation

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import LoginPage from './pages/auth/login/LoginPage'
+
+function App() {
+ 
+
+  return (
+    <>
+      <LoginPage></LoginPage>
+    </>
+  )
+}
+
+export default App
---
+++ my-app/src/pages/auth/login/LoginPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './LoginPage.css'
+
+function LoginPage() {
+
+
+  return (
+    <>
+        <h1>LoginWorks</h1>
+    </>
+  )
+}
+
+export default LoginPage
