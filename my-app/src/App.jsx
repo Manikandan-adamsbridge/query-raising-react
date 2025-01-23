670b9d217f004bbdac6b5ba8f5392c8e8d2a8cbb
ManiKandan2129 <gokulvirat2129@gmail.com>
2025-01-23 22:26:35 +0530

    first

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import LoginPage from './pages/auth/login/LoginPage'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
  
--- my-app/src/pages/auth/login/LoginPage.jsx
+++ my-app/src/pages/auth/login/LoginPage.jsx
@@ -1,12 +1,43 @@
 import React from 'react'
 import './LoginPage.css'
+import Form from 'react-bootstrap/Form';
+
 
 function LoginPage() {
 
 
   return (
     <>
-        <h1>LoginWorks</h1>
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className="col-12 full-page">
+            <div className='login-container'>
+              <div className="row">
+                <div className="col-6"></div>
+                <div className="col-6">
+                <h4 className='text-center mb-4'>Zen Query Ticketing System</h4>
+                  <Form.Floating className="mb-3">
+                    <Form.Control
+                      id="floatingInputCustom"
+                      type="email"
+                      placeholder="name@example.com"
+                    />
+                    <label htmlFor="floatingInputCustom">Email address</label>
+                    </Form.Floating>
+                    <Form.Floating>
+                    <Form.Control
+                      id="floatingPasswordCustom"
+                      type="password"
+                      placeholder="Password"
+                    />
+                    <label htmlFor="floatingPasswordCustom">Password</label>
+                  </Form.Floating>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
