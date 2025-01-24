f755832f7ed32dfbcd5f76111088b9daaaf74cbd
ManiKandan2129 <gokulvirat2129@gmail.com>
2025-01-24 22:36:52 +0530

    login ui

--- my-app/src/pages/auth/login/LoginPage.jsx
+++ my-app/src/pages/auth/login/LoginPage.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './LoginPage.css'
 import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faUser } from '@fortawesome/free-regular-svg-icons';
+import Button from 'react-bootstrap/Button';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 
 function LoginPage() {
@@ -15,23 +20,38 @@ function LoginPage() {
               <div className="row">
                 <div className="col-6"></div>
                 <div className="col-6">
-                <h4 className='text-center mb-4'>Zen Query Ticketing System</h4>
-                  <Form.Floating className="mb-3">
-                    <Form.Control
-                      id="floatingInputCustom"
-                      type="email"
-                      placeholder="name@example.com"
-                    />
-                    <label htmlFor="floatingInputCustom">Email address</label>
-                    </Form.Floating>
-                    <Form.Floating>
-                    <Form.Control
-                      id="floatingPasswordCustom"
-                      type="password"
-                      placeholder="Password"
-                    />
-                    <label htmlFor="floatingPasswordCustom">Password</label>
-                  </Form.Floating>
+                  <div className="login-form">
+                  <h4 className='text-center mb-4'>Zen Query Ticketing System</h4>
+        
+                    <InputGroup className="mb-4" type="email">
+                      <InputGroup.Text>
+                      <FontAwesomeIcon icon={faUser} />
+                      </InputGroup.Text>
+                      <Form.Control id="inlineFormInputGroup" placeholder="Email address" />
+                    </InputGroup>
+
+                    <InputGroup className="mb-1" type="password">
+                      <InputGroup.Text>
+                      <FontAwesomeIcon icon={faEnvelope} />
+                      </InputGroup.Text>
+                      <Form.Control id="inlineFormInputGroup" placeholder="Password" />
+                    </InputGroup>
+
+                    <div className="sub d-flex justify-content-end me-2 mb-4">
+                        <a href="#">Forgot password?</a>
+                      </div>
+
+                    <div className="btn-container mb-4">
+                      <Button variant="outline-dark" className='w-100'>
+                        <FontAwesomeIcon icon={faGoogle} />
+                      </Button>
+                      <Button variant="primary" className='w-100'>Login</Button>
+                    </div>
+
+                    <div className="footer-container ">
+                     <p className='fw-semibold'>First time here? <a className='text-primary'>Sign up for free</a></p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
