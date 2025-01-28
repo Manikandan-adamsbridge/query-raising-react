b2e2fc19fc2cdd9a08793c7486297b9f131ec3fb
ManiKandan2129 <gokulvirat2129@gmail.com>
2025-01-28 23:26:08 +0530

    sidebar

--- my-app/src/components/sidebar/Sidebar.jsx
+++ my-app/src/components/sidebar/Sidebar.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Sidebar.css'
 import { faClipboard } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAddressBook, faBarsProgress, faBook, faCalendarDays, faHouse, faListCheck, faSquarePlus, faUserPen } from '@fortawesome/free-solid-svg-icons';
+
 
 function Sidebar() {
   return (
@@ -9,17 +11,49 @@ function Sidebar() {
         <div className="container">
             <div className="sidebar">
                 <div className="sidebar-content">
+                    <div className="menu-logo">
+                        <img src='../../../public/zen-logo.png' alt='logo-img' className='logo-zen'></img>
+                        <span className="text">Student</span>
+                    </div>
                     <div className="menu-item">
-                        <FontAwesomeIcon icon={faClipboard} className="icon"/>
+                        <FontAwesomeIcon icon={faHouse} className="icon-s"/>
                         <span className="text">Home</span>
                     </div>
                     <div className="menu-item">
-                        <FontAwesomeIcon icon={faClipboard} className="icon"/>
-                        <span className="text">Profile</span>
+                        <FontAwesomeIcon icon={faSquarePlus} className="icon-s"/>
+                        <span className="text">Create Query</span>
                     </div>
                     <div className="menu-item">
-                        <FontAwesomeIcon icon={faClipboard} className="icon"/>
-                        <span className="text">Settings</span>
+                        <FontAwesomeIcon icon={faAddressBook} className="icon-s"/>
+                        <span className="text">Query</span>
+                    </div>
+                    <div className="menu-item">
+                        <FontAwesomeIcon icon={faCalendarDays} className="icon-s"/>
+                        <span className="text">Class</span>
+                    </div>
+                    <div className="menu-item">
+                        <FontAwesomeIcon icon={faBarsProgress} className="icon-s"/>
+                        <span className="text">Progress</span>
+                    </div>
+                    <div className="menu-item">
+                        <FontAwesomeIcon icon={faUserPen} className="icon-s"/>
+                        <span className="text">Edit User</span>
+                    </div>
+                    <div className="menu-item">
+                        <FontAwesomeIcon icon={faListCheck} className="icon-s"/>
+                        <span className="text">Task</span>
+                    </div>
+                    <div className="menu-item">
+                        <FontAwesomeIcon icon={faBook} className="icon-s"/>
+                        <span className="text">Certificate</span>
+                    </div>
+                    <div className="menu-item">
+                        <FontAwesomeIcon icon={faAddressBook} className="icon-s"/>
+                        <span className="text">project</span>
+                    </div>
+                    <div className="menu-item">
+                        <FontAwesomeIcon icon={faCalendarDays} className="icon-s"/>
+                        <span className="text">Leave Application</span>
                     </div>
                 </div>
             </div>
